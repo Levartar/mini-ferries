e0640164158b0b670f039835792ca02b415e53f0
Eni <erik.nikiforov@gmail.com>
2026-03-05 21:05:09 +0200

    Tried to simplify Ship logic
    I tried to make max_ports visible but read-only for the Godot Inspector to test if a changing ship name in the inspector also changes the value. I have no idea if it works, as I cannot make the value read-only for the inspector.

--- src/scripts/ship.gd
+++ src/scripts/ship.gd
@@ -1,34 +1,43 @@
+@tool
 extends Area2D
 class_name Ship
 
-enum ShipTypes {TWO, THREE}
 enum ShipNames {QUEEN, PRINCESS, MEGASTAR}
 
-@export var ship_name: ShipNames = ShipNames.QUEEN
-var ship_type: ShipTypes = ShipTypes.TWO
-var max_ports: int = 2
-
-static var ShipTags = {
-	ShipNames.QUEEN: "Baltic Queen",
-	ShipNames.PRINCESS: "Baltic Princess",
-	ShipNames.MEGASTAR: "Megastar"
-}
-
-static var ShipIcons = {
-	ShipNames.QUEEN: preload("res://assets/Ships/baltic_queen.png"),
-	ShipNames.PRINCESS: preload("res://assets/Ships/baltic_princess.png"),
-	ShipNames.MEGASTAR: preload("res://assets/Ships/megastar.png")
-}
-const SHIP_ICON = preload("res://assets/sprites/icons/tracking_horizontal.png")
-
+@export var ship_name: ShipNames:
+	set(value):
+		ship_name = value
+		_update_info()
+		notify_property_list_changed()
+		
+@export var max_ports: int = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Sprite2D.texture = ShipIcons.get(ship_name, preload("res://assets/sprites/icons/tracking_horizontal.png"))
-	$Label.text = ShipTags.get(ship_name, "What Is This?")
-	if ship_name != ShipNames.PRINCESS:
-		ship_type = ShipTypes.THREE
-		max_ports = 3
+	_update_info()
 
+func _update_info():
+	match ship_name:
+		ShipNames.QUEEN:
+			$Sprite2D.texture = preload("res://assets/Ships/baltic_queen.png")
+			$Label.text = "Baltic Queen"
+			max_ports = 3
+		ShipNames.PRINCESS:
+			$Sprite2D.texture = preload("res://assets/Ships/baltic_princess.png")
+			$Label.text = "Baltic Princess"
+			max_ports = 3
+		ShipNames.MEGASTAR:
+			$Sprite2D.texture = preload("res://assets/Ships/megastar.png")
+			$Label.text = "Megastar"
+			max_ports = 2 
+	notify_property_list_changed()
+
+# ENi note: Gemini says that this makes the variable grayed out/read-only in the UI
+# This is the "Magic" function for Godot 4 stable
+func _validate_property(property: Dictionary):
+	if property.name == "max_ports":
+		# This adds the 'Read Only' flag to the existing export
+		property.usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_READ_ONLY
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
